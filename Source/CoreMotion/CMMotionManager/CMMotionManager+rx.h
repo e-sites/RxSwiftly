a0ad915ebccdb0efe3660c5e4b23910946931e38
Bas van Kuijck <bas@e-sites.nl>
2017-08-02 08:48:23 +0200

    Header files

---
+++ Source/CoreMotion/CMMotionManager/CMMotionManager+rx.h
@@ -0,0 +1,19 @@
+//
+//  CMMotionManager+rx.h
+//  CMMotionManager+rx
+//
+//  Created by Bas van Kuijck on 31/07/2017
+//  Copyright © 2017 E-Sites. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for CMMotionManager+rx.
+FOUNDATION_EXPORT double CMMotionManager_rxVersionNumber;
+
+//! Project version string for CMMotionManager+rx.
+FOUNDATION_EXPORT const unsigned char CMMotionManager_rxVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CMMotionManager_rx/PublicHeader.h>
+
+
